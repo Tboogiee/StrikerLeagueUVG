7fb87d76cb6d9f5cf07d5b9304b0a7930f66e281
Tboogiee <105893657+Tboogiee@users.noreply.github.com>
2023-10-06 14:30:42 -0600

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,11 @@
+function initializeTeams() {
+    const teams = [
+        { name: document.getElementById('team1').value, points: 0, goalDifference: 0 },
+        { name: document.getElementById('team2').value, points: 0, goalDifference: 0 },
+        { name: document.getElementById('team3').value, points: 0, goalDifference: 0 },
+        { name: document.getElementById('team4').value, points: 0, goalDifference: 0 }
+    ];
+    // Generate matches and display them...
+    // This is just a start. You'll need to add functionality to input match results,
+    // calculate points, display the bracket, and so on.
+}
